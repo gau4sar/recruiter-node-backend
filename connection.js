a5c8ec2879499eb21843a237bed8d7b6ce8e4b40
Gaurav Sarma <gaurav4sarma@gmail.com>
2020-04-08 04:35:48 +0530

    Integrated DB connection to node

---
+++ connection.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql");
+
+var mysqlConnection = mysql.createConnection({
+    /* host: "eu-cdbr-west-02.cleardb.net",
+    user: "b2eb589c13b38e",
+    password: "7bf88910",
+    database: "heroku_ba4e1a29199c2ca", */
+    host: "127.0.0.1",
+    user: "root",
+    password: "Gappu@1234",
+    database: "recruiters",
+    multipleStatements: true
+});
+
+mysqlConnection.connect((err)=> {
+    if(!err) {
+        console.log("Connected to mysql db");
+    } else{
+        console.log("Connection failed !!!" + err.message);
+    }
+});
+
+module.exports = mysqlConnection
